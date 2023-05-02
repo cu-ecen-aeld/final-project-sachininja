d2134c2fd06812d2070e9e10a12413482c8f14c1
sachininja <sama5040@colorado.edu>
2023-05-02 05:54:42 +0000

    Server Buildroot

--- base_external/package/bluetooth_server/bluetooth_server.mk
+++ base_external/package/bluetooth_server/bluetooth_server.mk
@@ -1,21 +1,21 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# BLUETOOTH_SERVER
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AUDIOBT_PACKAGE_VERSION = '78b12b43138a9a4012d168822c337a9919c40faf'
+#TODO: Fill up the contents below in order to reference bluetooth server pacakages
+BLUETOOTH_SERVER_VERSION = '003b8ec33d9286394494a2aae8d7903d3bac348a'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AUDIOBT_PACKAGE_SITE = 'git@github.com:cu-ecen-aeld/final-project-suja8742.git'
-AUDIOBT_PACKAGE_SITE_METHOD = git
-AUDIOBT_PACKAGE_GIT_SUBMODULES = YES
+BLUETOOTH_SERVER_SITE = 'git@github.com:cu-ecen-aeld/final-project-suja8742.git'
+BLUETOOTH_SERVER_SITE_METHOD = git
+BLUETOOTH_SERVER_GIT_SUBMODULES = YES
 
-define AUDIOBT_PACKAGE_INSTALL_TARGET_CMDS
+define BLUETOOTH_SERVER_INSTALL_TARGET_CMDS
         $(INSTALL) -m 0755 $(@D)/server_script.py $(TARGET_DIR)/usr/bin
 endef
 $(eval $(generic-package))
--- base_external/package/i2s_package/i2s_package.mk
+++ base_external/package/i2s_package/i2s_package.mk
@@ -1,12 +1,12 @@
 
 ##############################################################
 #
-# AESD-ASSIGNMENTS
+# I2S_PACKAGE
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-I2S_PACKAGE_VERSION = 'f4cce6e8ccdb2bc659ca24b2d8e1604536143bc3'
+#TODO: Fill up the contents below in order to reference i2s database 
+I2S_PACKAGE_VERSION = 'ba750713602c17231a1b4bac477fcaecfdb70522'
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
@@ -21,6 +21,5 @@ define I2S_PACKAGE_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/test3.wav $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/test4.wav $(TARGET_DIR)/usr/bin
         $(INSTALL) -m 0755 $(@D)/i2s_sequencer.py $(TARGET_DIR)/usr/bin
-        $(INSTALL) -m 0755 $(@D)/server_script.py $(TARGET_DIR)/usr/bin
 endef
 $(eval $(generic-package))
